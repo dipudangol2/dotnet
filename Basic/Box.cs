329f7bd61fd5a3bac98c2f8c6c1eb04a0cfce3c9
dipudangol2 <dipudangol2@gmail.com>
2024-11-25 10:42:33 +0545

    Learn Classes, Objects, namespace

---
+++ Basic/Box.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geoshape
+{
+    internal class Box
+    {
+
+        int length, breadth, height;
+        public Box(int length, int breadth, int height)
+        {
+            Console.WriteLine("Box constructed");
+            this.length = length;
+            this.breadth = breadth;
+            this.height = height;
+        }
+        private int CalculateArea()
+        {
+            return length * breadth;
+        }
+        public int PrintArea()
+        {
+            return CalculateArea();
+        }
+        //Function can be called directly with className without creating an object of the class 
+        public static void PrintMe()
+        {
+            Console.WriteLine("printme");
+        }
+    }
+}
--- Basic/Program.cs
+++ Basic/Program.cs
@@ -1,42 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+using Geoshape; //importing Geoshape namespace
 namespace Basic
 {
+
+    //class initialization
+    class SimpleInterest
+    {
+        double principal, rate, time;
+
+        public SimpleInterest(double principal, double rate, double time)
+        {
+            this.principal = principal;
+            this.rate = rate;
+            this.time = time;
+        }
+        private double CalculateInterest()
+        {
+            return this.principal * this.rate * this.time;
+}
+        public void PrintInterest()
+        {
+            Console.WriteLine($" The interest for Rs.{this.principal} for {this.time} years with rate {this.rate} per annum is {this.CalculateInterest()}");
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            int firstNumber = 1;
-            int secondNumber = 2;
-
-            Console.WriteLine("First:{0}, Second:{1}", firstNumber, secondNumber);
-            Console.WriteLine("After Swap");
-            //Swapping without temporary variable 
-            firstNumber = firstNumber + secondNumber;
-            secondNumber = firstNumber - secondNumber;
-            firstNumber = firstNumber - secondNumber;
-
-            Console.WriteLine("First: {0}, Second: {1}", firstNumber, secondNumber);
-            int counter = 0;
-            //print 3X3 squares of *
-            for (counter = 0; counter < 3; counter++)
-            {
-                Console.WriteLine("*    *    *");
-            }
-            Console.WriteLine("\n");
-
-            for (counter = 0; counter < 3; counter++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    Console.Write("*\t");
-                }
-                Console.WriteLine("\n");
-            }
+            SimpleInterest I = new SimpleInterest(150000, 5.9, 3);
+            I.PrintInterest();
+            
+            Box box = new Box(5, 7, 9);
+            Console.WriteLine(box.PrintArea());
+            Box.PrintMe();
+            
             Console.ReadLine();
         }
     }
